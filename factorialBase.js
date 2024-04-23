a95bbf05ef672d93d57b5ffec85a9a1fc249d85d
Ryan1729 <Ryan1729@gmail.com>
2024-04-23 01:06:00 -0600

    placeholder for factorial number impl

---
+++ factorialBase.js
@@ -0,0 +1,16 @@
+var FactorialBase = (function () {
+    "use strict";
+
+    const of = (n) => {
+        return [] // TODO
+    }
+
+    const toString = (factorialBaseN) => {
+        return factorialBaseN.join("")
+    }
+
+    return {
+        of,
+        toString,
+    }
+}())
